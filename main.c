e041bf7d2e1b92e139c141050ba84823e6585196
ZeDaAreia <kauanenriquebp@gmail.com>
2026-09-20 17:25:51 -0400

    Implementação parte aluno 1

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include "estoque.h"
+
 void exibir_menu(void) {
 printf("\n=== CONTROLE DE ESTOQUE ===\n");
 printf("1 - Listar produtos\n");
@@ -8,13 +9,15 @@ printf("2 - Exibir valor total em estoque\n");
 printf("0 - Sair\n");
 printf("Escolha uma opcao: ");
 }
+
 void listar_produtos(Produto lista[], int total) {
-printf("\n--- Produtos Cadastrados ---\n");
-for (int i = 0; i < total; i++) {
-// BUG: esqueceram de imprimir o ID e a quebra de linha está inadequada
-printf("Nome: %s | Preco: R$ %.2f | Qtd: %d", lista[i].nome,
-lista[i].preco, lista[i].quantidade);
-}
+    printf("\n--- Produtos Cadastrados ---\n");
+    for (int i = 0; i < total; i++) {
+        // BUG: esqueceram de imprimir o ID e a quebra de linha está inadequada
+        printf("ID: %d | Cod: %s | Nome: %s | Preco: R$ %.2f | Qtd: %d\n", 
+               lista[i].id, lista[i].codigo_barras, lista[i].nome, lista[i].preco, lista[i].quantidade);
+    }
+
 }
 float calcular_total(Produto lista[], int total) {
 float soma = 0.0;
@@ -24,14 +27,19 @@ soma += lista[i].preco;
 }
 return soma;
 }
+
 int main(void) {
 Produto estoque[MAX_ITENS];
 int total_produtos = 2;
+
+// Caderno
 estoque[0].id = 1;
+strcpy(estoque[0].codigo_barras, "7890001");
 strcpy(estoque[0].nome, "Caderno");
 estoque[0].preco = 15.50;
 estoque[0].quantidade = 10;
 estoque[1].id = 2;
+strcpy(estoque[1].codigo_barras, "7890002");
 strcpy(estoque[1].nome, "Caneta");
 estoque[1].preco = 3.00;
 estoque[1].quantidade = 50;
