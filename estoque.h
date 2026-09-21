ffbd1d453cd95d970a72830b37a6e2080d42be9d
ZeDaAreia <kauanenriquebp@gmail.com>
2026-09-21 15:45:48 -0400

    Adicionamento de taxa de desconto

--- estoque.h
+++ estoque.h
@@ -4,6 +4,7 @@
 #define MAX_ITENS 50           
 #define ESTOQUE_MINIMO 5        
 #define TAXA_PADRAO 0.10
+#define TAXA_DESCONTO 0.05
 typedef struct {
     int id;
     char categoria[20];  
@@ -16,5 +17,6 @@ typedef struct {
 void exibir_menu(void);
 void listar_produtos(Produto lista[], int total);
 float calcular_total(Produto lista[], int total);
+float aplicar_desconto(float total);
 
 #endif
