2ef09e5537a0cfadadd389e4beb26aa89365cd37
ZeDaAreia <kauanenriquebp@gmail.com>
2026-09-21 16:00:04 -0400

    ADD no switch case 3 e função de juros

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ void exibir_menu(void) {
     printf("1 - Listar produtos\n");
     printf("2 - Exibir total em estoque (com tributos)\n");
     printf("3 - Exibir total com desconto a vista\n");
+    printf("4 - Exibir total a prazo (com juros)\n");
     printf("0 - Sair\n");
     printf("Escolha uma opcao: ");
 }
@@ -69,6 +70,11 @@ int main(void) {
                 printf("\nTotal com desconto a vista: R$ %.2f\n", total_a_vista);
                 break;
             }
+            case 4: {
+                float total = calcular_total(estoque, qtd);
+                printf("Total a prazo: R$ %.2f\n", aplicar_juros(total));
+                break;
+}
             case 0:
                 printf("\nEncerrando o programa...\n");
                 break;
@@ -82,4 +88,7 @@ int main(void) {
    
  float aplicar_desconto(float total) {
     return total - (total * TAXA_DESCONTO);
+}
+float aplicar_juros(float total) {
+    return total + (total * TAXA_JUROS);
 }
